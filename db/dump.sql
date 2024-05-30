820d87b83d60a1482059f890a5dfa0c0c5208826
Hindie <hindiesuputra@gmail.com>
2024-05-30 23:31:03 +1000

    feat: add goddle_stats table into db

--- db/dump.sql
+++ db/dump.sql
@@ -1,10 +1,15 @@
-DO $$ 
-BEGIN
-    IF NOT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'users') THEN
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            email VARCHAR(100) UNIQUE NOT NULL,
-            password VARCHAR(100) NOT NULL
-        );
-    END IF;
-END $$;
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS goddle_stats (
+    stats_id SERIAL,
+    user_id INT NOT NULL,
+    top_attempt INT DEFAULT 0,
+    top_time INT DEFAULT 0,
+    games_played INT DEFAULT 0,
+    PRIMARY KEY (stats_id, user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
